523de5b5cee71a74d8a2b2889d83b234a5c517e6
BM5k <me@bm5k.com>
2012-04-12 16:02:21 -0700

    add admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120412215809) do
+ActiveRecord::Schema.define(:version => 20120412225630) do
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.integer  "item"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.string   "table"
+    t.string   "username"
+    t.text     "message"
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.boolean  "admin",                  :default => false, :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -24,8 +38,8 @@ ActiveRecord::Schema.define(:version => 20120412215809) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
