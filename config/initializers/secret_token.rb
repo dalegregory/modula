911ab3c57d28c2892dcaab82fe1df9a1169c4046
BM5k <me@bm5k.com>
2012-04-12 14:58:13 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Modula::Application.config.secret_token = '8d6c1fe042c679c7b7ad340073acfc005c739ab537faa5242232f0aaf40d270cf14aab1b075482f4adf35a21af34b863a0f9398f48c808158465441221382f95'
